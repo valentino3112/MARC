7c79c3c9a1c9e0f544dcd84031e893add2f08503
Victor de Laportalière <victor.taillefer-de-laportaliere@efrei.net>
2024-11-12 18:25:59 +0100

    création des fonctions free node et free arbre  et find min

--- arbre.c
+++ arbre.c
@@ -32,3 +32,55 @@ int add_node(node_t* node, node_t* child){
     return 1;
 
 }
+
+
+void free_node(node_t* node) {
+    if(node==NULL)
+        return;
+    for (int i = 0; i < MAX_CHILDREN; i++) {
+        if(node->children[i]!=NULL)
+            free_node(node->children[i]);
+    }
+    free(node);
+}
+
+node_t* find_min(node_t* node) {
+    node_t* min = node;
+    int min_cost = min->cost;
+    for (int i = 0; i < MAX_CHILDREN; i++)
+        {
+        if(node->children[i]!=NULL)
+        {
+            if(node->children[i]->cost<min_cost) {
+                min = node->children[i];
+                min_cost = node->cost;
+            }
+        }
+    }
+    return min;
+}
+
+
+
+
+
+
+arbre_t* create_arbre() {
+    arbre_t* new_arbre = malloc(sizeof(arbre_t));
+    if (new_arbre == NULL) {
+        printf("Erreur\n");
+        return NULL;
+    }
+    new_arbre->root=NULL;
+    return new_arbre;
+}
+
+
+void free_arbre(arbre_t* arbre) {
+
+    if (arbre == NULL)
+        return;
+    free_node(arbre->root);
+    free(arbre);
+}
+

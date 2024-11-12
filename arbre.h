bb7aba426773ebaf0c639ef05fb4a4c5886ff171
Victor de Laportalière <victor.taillefer-de-laportaliere@efrei.net>
2024-11-12 16:51:08 +0100

    création des structure arbres et node pour representer les arbres de possibilite de mouvement

--- arbre.h
+++ arbre.h
@@ -4,5 +4,19 @@
 
 #ifndef ARBRE_H
 #define ARBRE_H
+#include "moves.h"
+#define MAX_CHILDREN 9
+
+
+typedef struct node_s {
+    int cost;
+    t_move move;
+    struct node_s* children[MAX_CHILDREN];
+}node_t;
+
+typedef struct arbre_s {
+    node_t* root;
+} arbre_t;
+
 
 #endif //ARBRE_H

b8abfd52e86c299e81c872e66ae224407d527912
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-11-25 09:41:04 +0100

    Update map.c

--- map.c
+++ map.c
@@ -2,355 +2,57 @@
 // Created by flasque on 19/10/2024.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "map.h"
-#include "loc.h"
-#include "queue.h"
+#ifndef UNTITLED1_MAP_H
+#define UNTITLED1_MAP_H
 
-/* prototypes of local functions */
-/* local functions are used only in this file, as helper functions */
+#define COST_UNDEF 65535
+/**
+ * @brief Enum for the possible soils of the map
+ */
+typedef enum e_soil
+{
+    BASE_STATION,
+    PLAIN,
+    ERG,
+    REG,
+    CREVASSE
+} t_soil;
 
 /**
- * @brief :  function to get the position of the base station
- * @param map : the map
- * @return : the position of the base station
+ * @brief Array of costs for the soils
  */
-t_position getBaseStationPosition(t_map);
+static const int _soil_cost[5] = {0, 1, 2, 4, 10000};
 
 /**
- * @brief : function to calculate costs of the map  from the base station
- * @param map : the map
- * @return none
+ * @brief Structure for the map
+
  */
-void calculateCosts(t_map);
+typedef struct s_map
+{
+    t_soil  **soils;
+    int     **costs;
+    int     x_max;
+    int     y_max;
+} t_map;
 
 /**
- * @brief : function to remove 'false' crevasses costs from the costs array
- * @param map : the map
- * @return none
+ * @brief Function to initialise the map from a file
+ * @param filename : the name of the file
+ * @return the map
  */
-void removeFalseCrevasses(t_map);
+t_map createMapFromFile(char *);
 
-/* definition of local functions */
+/**
+ * @brief Function to create a standard training map (11x11 with only plains and base station in the middle)
+ * @param none
+ * @return a standard map
+ */
+ t_map createTrainingMap();
 
+/**
+ * @brief display the map with characters
+ * @param map : the map to display
+ */
+void displayMap(t_map);
 
-/*----------------------------------------------------------------------------------------------------------------------
-                            FONCTION : getBaseStationPosition
-Cette fonction recherche la position de la station de base dans la carte (map). Elle parcourt chaque cellule jusqu'�
-trouver la position marqu�e comme station de base.
-Elle prend en param�tre :
-  - `map` : la carte de type `t_map`.
-Elle renvoie la position de la station de base sous forme de `t_position`.
-Si aucune station de base n'est trouv�e, elle affiche un message d'erreur et termine le programme.
-----------------------------------------------------------------------------------------------------------------------*/
-t_position getBaseStationPosition(t_map map)
-{
-    t_position pos;
-    int i = 0;
-    int found = 0;
-    while (i < map.y_max && !found)
-    {
-        int j = 0;
-        while (j < map.x_max && !found)
-        {
-            if (map.soils[i][j] == BASE_STATION)
-            {
-                pos.x = j;
-                pos.y = i;
-                found = 1;
-            }
-            j++;
-        }
-        i++;
-    }
-    // if the base station is not found, we exit the program
-    if (!found)
-    {
-        fprintf(stderr, "Error: base station not found in the map\n");
-        exit(1);
-    }
-    return pos;
-}
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-                            FONCTION : removeFalseCrevasses
-Cette fonction supprime les faux co�ts de crevasse dans la carte. Elle ajuste les co�ts des cases incorrectement marqu�es
-pour �viter toute confusion dans les calculs de co�t.
-Elle prend en param�tre :
-  - `map` : la carte de type `t_map`.
-Elle ne renvoie rien et modifie directement le tableau `map.costs`.
-----------------------------------------------------------------------------------------------------------------------*/
-void removeFalseCrevasses(t_map map)
-{
-    // step 1 : find the minimal cost > 10000 in the costs array where the soil is not a crevasse
-    int over=0;
-    int imin, jmin;
-    while (!over)
-    {
-        int min_cost = COST_UNDEF;
-        imin = map.y_max;
-        jmin = map.x_max;
-        for (int i=0; i<map.y_max; i++)
-        {
-            for (int j=0; j<map.x_max; j++)
-            {
-                if (map.soils[i][j] != CREVASSE && map.costs[i][j] > 10000 && map.costs[i][j] < min_cost)
-                {
-                    min_cost = map.costs[i][j];
-                    imin = i;
-                    jmin = j;
-                }
-            }
-        }
-        if (imin < map.y_max && jmin < map.x_max)
-        {
-            // step 2 : calculate the costs of the neighbours of the position
-            t_position pos;
-            pos.x = jmin;
-            pos.y = imin;
-            t_position lp, rp, up, dp;
-            lp = LEFT(pos);
-            rp = RIGHT(pos);
-            up = UP(pos);
-            dp = DOWN(pos);
-            int min_neighbour = COST_UNDEF;
-            if (isValidLocalisation(lp, map.x_max, map.y_max))
-            {
-                min_neighbour = (map.costs[lp.y][lp.x] < min_neighbour) ? map.costs[lp.y][lp.x] : min_neighbour;
-            }
-            if (isValidLocalisation(rp, map.x_max, map.y_max))
-            {
-                min_neighbour = (map.costs[rp.y][rp.x] < min_neighbour) ? map.costs[rp.y][rp.x] : min_neighbour;
-            }
-            if (isValidLocalisation(up, map.x_max, map.y_max))
-            {
-                min_neighbour = (map.costs[up.y][up.x] < min_neighbour) ? map.costs[up.y][up.x] : min_neighbour;
-            }
-            if (isValidLocalisation(dp, map.x_max, map.y_max))
-            {
-                min_neighbour = (map.costs[dp.y][dp.x] < min_neighbour) ? map.costs[dp.y][dp.x] : min_neighbour;
-            }
-            int self_cost = _soil_cost[map.soils[imin][jmin]];
-            map.costs[imin][jmin] = (min_neighbour + self_cost < map.costs[imin][jmin]) ? min_neighbour + self_cost : map.costs[imin][jmin];
-        }
-        else
-        {
-            over = 1;
-        }
-    }
-}
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-                            FONCTION : calculateCosts
-Cette fonction calcule les co�ts de d�placement dans la carte � partir de la station de base en utilisant une approche
-de parcours en largeur. Elle utilise les co�ts du sol pour �tablir les co�ts de chaque position sur la carte.
-Elle prend en param�tre :
-  - `map` : la carte de type `t_map`.
-Elle ne renvoie rien et modifie directement le tableau `map.costs`.
-----------------------------------------------------------------------------------------------------------------------*/
-void calculateCosts(t_map map)
-{
-    t_position baseStation = getBaseStationPosition(map);
-    //create a queue to store the positions to visit
-    t_queue queue = createQueue(map.x_max * map.y_max);
-    //enqueue the base station
-    enqueue(&queue, baseStation);
-    // while the queue is not empty
-    while (queue.first != queue.last)
-    {
-        // dequeue the position
-        t_position pos = dequeue(&queue);
-        // get its self cost
-        int self_cost = _soil_cost[map.soils[pos.y][pos.x]];
-        // get ts neighbours
-        t_position lp, rp, up, dp;
-        lp = LEFT(pos);
-        rp = RIGHT(pos);
-        up = UP(pos);
-        dp = DOWN(pos);
-        // get the mimimum cost of the neighbours
-        int min_cost = COST_UNDEF;
-        if (isValidLocalisation(lp, map.x_max, map.y_max))
-        {
-            min_cost = (map.costs[lp.y][lp.x] < min_cost) ? map.costs[lp.y][lp.x] : min_cost;
-        }
-        if (isValidLocalisation(rp, map.x_max, map.y_max))
-        {
-            min_cost = (map.costs[rp.y][rp.x] < min_cost) ? map.costs[rp.y][rp.x] : min_cost;
-        }
-        if (isValidLocalisation(up, map.x_max, map.y_max))
-        {
-            min_cost = (map.costs[up.y][up.x] < min_cost) ? map.costs[up.y][up.x] : min_cost;
-        }
-        if (isValidLocalisation(dp, map.x_max, map.y_max))
-        {
-            min_cost = (map.costs[dp.y][dp.x] < min_cost) ? map.costs[dp.y][dp.x] : min_cost;
-        }
-        // the cost of the current position is the minimum cost of the neighbours + 1 or 0 if the soil is a base station
-        map.costs[pos.y][pos.x] = (map.soils[pos.y][pos.x] == BASE_STATION) ? 0 : min_cost + self_cost;
-        // enqueue the neighbours if they are not visited yet
-        if (isValidLocalisation(lp, map.x_max, map.y_max) && map.costs[lp.y][lp.x] == COST_UNDEF)
-        {
-            // mark as visited - change the cost to 65534
-            map.costs[lp.y][lp.x] = COST_UNDEF-1;
-            enqueue(&queue, lp);
-        }
-        if (isValidLocalisation(rp, map.x_max, map.y_max) && map.costs[rp.y][rp.x] == COST_UNDEF)
-        {
-            map.costs[rp.y][rp.x] = COST_UNDEF-1;
-            enqueue(&queue, rp);
-        }
-        if (isValidLocalisation(up, map.x_max, map.y_max) && map.costs[up.y][up.x] == COST_UNDEF)
-        {
-            map.costs[up.y][up.x] = COST_UNDEF-1;
-            enqueue(&queue, up);
-        }
-        if (isValidLocalisation(dp, map.x_max, map.y_max) && map.costs[dp.y][dp.x] == COST_UNDEF)
-        {
-            map.costs[dp.y][dp.x] = COST_UNDEF-1;
-            enqueue(&queue, dp);
-        }
-    }
-
-
-    return;
-}
-/* definition of exported functions */
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-                            FONCTION : createMapFromFile
-Cette fonction cr�e une carte (map) � partir d'un fichier en respectant les r�gles de format de fichier. Elle initialise
-les sols et les co�ts en fonction des valeurs lues et appelle `calculateCosts` et `removeFalseCrevasses`.
-Elle prend en param�tre :
-  - `filename` : nom du fichier contenant les informations de la carte.
-Elle renvoie une structure `t_map` remplie.
-----------------------------------------------------------------------------------------------------------------------*/
-t_map createMapFromFile(char *filename)
-{
-    /* rules for the file :
-     * - the first line contains the number of lines : y dimension (int)
-     * - the second line contains the number of columns : x dimension (int)
-     * - the next lines contain the map values (int) separated by spaces : one line = one row
-     * - the values are the following : 0 = BASE_STATION, 1 = PLAIN, 2 = ERG, 3 = REG, 4 = CREVASSE
-     */
-
-    t_map map;
-    int xdim, ydim;     // dimensions of the map
-    char buffer[100];   // buffer for reading the file line by line
-
-    FILE *file = fopen(filename,"rt");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: cannot open file %s\n", filename);
-        exit(1);
-    }
-    fscanf(file, "%d", &ydim);
-    fscanf(file, "%d", &xdim);
-    map.x_max = xdim;
-    map.y_max = ydim;
-    map.soils = (t_soil **)malloc(ydim * sizeof(t_soil *));
-    for (int i = 0; i < ydim; i++)
-    {
-        map.soils[i] = (t_soil *)malloc(xdim * sizeof(t_soil));
-    }
-    map.costs = (int **)malloc(ydim * sizeof(int *));
-    for (int i = 0; i < ydim; i++)
-    {
-        map.costs[i] = (int *)malloc(xdim * sizeof(int));
-    }
-    for (int i = 0; i < ydim; i++)
-    {
-
-        // parse the line to get the values : 0 = BASE_STATION, 1 = PLAIN, 2 = ERG, 3 = REG, 4 = CREVASSE
-        // values are separated by spaces, so we use sscanf with %d to get the values
-        for (int j = 0; j < xdim; j++)
-        {
-            int value;
-            fscanf(file, "%d", &value);
-            map.soils[i][j] = value;
-            // cost is 0 for BASE_STATION, 65535 for other soils
-            map.costs[i][j] = (value == BASE_STATION) ? 0 : COST_UNDEF;
-        }
-
-    }
-    fclose(file);
-    calculateCosts(map);
-    removeFalseCrevasses(map);
-    return map;
-}
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-                            FONCTION : createTrainingMap
-Cette fonction cr�e une carte d'entra�nement en appelant `createMapFromFile` avec un fichier de carte pr�d�fini.
-Elle ne prend aucun param�tre.
-Elle renvoie une structure `t_map`.
-----------------------------------------------------------------------------------------------------------------------*/
-t_map createTrainingMap()
-{
-    return createMapFromFile("..\\maps\\training.map");
-}
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-                            FONCTION : displayMap
-Cette fonction affiche la carte (map) dans la console en repr�sentant chaque type de sol par un motif sp�cifique
-(3x3 caract�res). Elle utilise des caract�res symboliques pour repr�senter chaque type de terrain.
-Elle prend en param�tre :
-  - `map` : la carte de type `t_map` � afficher.
-Elle ne renvoie rien.
-----------------------------------------------------------------------------------------------------------------------*/
-void displayMap(t_map map)
-{
-    /** the rules for display are :
-     * display all soils with 3x3 characters
-     * characters are : B for base station, '-' for plain, '~' for erg, '^' for reg, ' ' for crevasse
-     */
-    for (int i = 0; i < map.y_max; i++)
-    {
-        for (int rep = 0; rep < 3; rep++)
-        {
-            for (int j = 0; j < map.x_max; j++)
-            {
-                char c[4];
-                switch (map.soils[i][j])
-                {
-                    case BASE_STATION:
-                        if (rep==1)
-                        {
-                            strcpy(c, " B ");
-                        }
-                        else
-                        {
-                            strcpy(c, "   ");
-                        }
-                        break;
-                    case PLAIN:
-                        strcpy(c, "---");
-                        break;
-                    case ERG:
-                        strcpy(c, "~~~");
-                        break;
-                    case REG:
-                        strcpy(c, "^^^");
-                        break;
-                    case CREVASSE:
-                        sprintf(c, "%c%c%c",219,219,219);
-                        break;
-                    default:
-                        strcpy(c, "???");
-                        break;
-                }
-                printf("%s", c);
-            }
-            printf("\n");
-        }
-
-    }
-    return;
-}
+#endif //UNTITLED1_MAP_H

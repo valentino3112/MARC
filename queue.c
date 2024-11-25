1027801947a54bb7a87c89e1037f3d1d68ab6e79
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2024-11-25 09:43:16 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -23,6 +23,16 @@ t_queue createQueue(int size)
     return queue;
 }
 
+
+/*----------------------------------------------------------------------------------------------------------------------
+                            FONCTION : enqueue
+Cette fonction ajoute un �l�ment `pos` � la fin de la file `p_queue`.
+Elle prend en param�tres :
+  - `p_queue` : un pointeur vers la file (`t_queue`) dans laquelle ins�rer l'�l�ment.
+  - `pos` : la position (`t_position`) � ajouter.
+Pr�requis : la file ne doit pas �tre pleine.
+La fonction ne renvoie rien ; elle met directement � jour la file.
+----------------------------------------------------------------------------------------------------------------------*/
 void enqueue(t_queue *p_queue, t_position pos)
 {
     // the queue must not be full
@@ -32,6 +42,15 @@ void enqueue(t_queue *p_queue, t_position pos)
     return;
 }
 
+
+/*----------------------------------------------------------------------------------------------------------------------
+                            FONCTION : dequeue
+Cette fonction retire et renvoie l'�l�ment en t�te de la file `p_queue`.
+Elle prend en param�tre :
+  - `p_queue` : un pointeur vers la file (`t_queue`) dans laquelle retirer l'�l�ment.
+Pr�requis : la file ne doit pas �tre vide.
+Elle renvoie l'�l�ment retir� (`t_position`).
+----------------------------------------------------------------------------------------------------------------------*/
 t_position dequeue(t_queue *p_queue)
 {
     // the queue must not be empty
